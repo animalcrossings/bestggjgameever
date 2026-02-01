58ff0f97a542b8eebc6f8b8fe9a96d58c62a84c3
eli <N/A>
2026-01-31 19:12:40 -0500

    Initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputActionReference moveAction;
+    public float moveSpeed;
+    public Transform TargetPosition;
+    public LayerMask whatStopsMovement;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        TargetPosition.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, TargetPosition.position, moveSpeed * Time.deltaTime);
+        print(TargetPosition.position);
+        //Debug.Log("did you make it here0");
+        if (Vector3.Distance(transform.position, TargetPosition.position) <= .05f)
+        {
+            //Debug.Log("did you make it here");
+            if (Mathf.Abs(moveAction.action.ReadValue<Vector2>().x) == 1f)
+            {
+                //print(moveAction.action.ReadValue<Vector2>().x);
+                if (!Physics2D.OverlapCircle(TargetPosition.position + new Vector3(moveAction.action.ReadValue<Vector2>().x, 0f, 0f), .2f, whatStopsMovement))
+                {
+                    TargetPosition.position += new Vector3(moveAction.action.ReadValue<Vector2>().x, 0f, 0f);
+            }
+        }
+            if (Mathf.Abs(moveAction.action.ReadValue<Vector2>().y) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(TargetPosition.position + new Vector3(0f, moveAction.action.ReadValue<Vector2>().y, 0f), .2f, whatStopsMovement))
+                {
+                    TargetPosition.position += new Vector3(0f, moveAction.action.ReadValue<Vector2>().y, 0f);
+                }
+            }
+        }
+    }   
+}
